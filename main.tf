b014b553ac9367a1ef44f99c581b9614160fc31b
Gnomina <60029541+Gnomina@users.noreply.github.com>
2023-06-12 21:45:53 +0300

    main.

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"  # Замените на подходящий CIDR-блок для вашей сети
 
   tags = {
-    Name = "MyVPC_1"
+    Name = "MyVPC_TesT"
   }
 }
 
@@ -18,6 +18,37 @@ resource "aws_subnet" "my_subnet" {
   cidr_block              = "10.0.1.0/24"  # Замените на подходящий CIDR-блок для вашего subnet
 
   tags = {
-    Name = "MySubnet_1"
+    Name = "MySubnet_TesT"
   }
+}
+
+# Создание интернет-шлюза
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "MyIGW_TesT"
+  }
+}
+
+# Создание маршрутной таблицы
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "MyRouteTable_Test"
+  }
+}
+
+# Создание маршрута для интернет-шлюза
+resource "aws_route" "internet_route" {
+  route_table_id         = aws_route_table.my_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.my_igw.id
+}
+
+# Присоединение Subnet к маршрутной таблице
+resource "aws_route_table_association" "subnet_association" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_route_table.id
 }
