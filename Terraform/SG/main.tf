5be1ff19c644127ef54d28f2e500d09632b81047
GnoM <gnomovi4@gmail.com>
2023-06-13 22:11:43 +0300

    .

--- Terraform/SG/main.tf
+++ Terraform/SG/main.tf
@@ -12,7 +12,7 @@ data "terraform_remote_state" "VPC" {
 resource "aws_security_group" "my_security_group" {
   name        = "${var.name}"
   description = "${var.description}"
-  vpc_id      = data.terraform_remote_state.VPC.outputs.VPC-ID
+  vpc_id      = data.terraform_remote_state.VPC.outputs.vpc_id
 
   ingress {
     from_port   = 22
