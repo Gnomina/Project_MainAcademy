6c7fee988a4045c11e7f7a5783486d9b3155ef84
GnoM <gnomovi4@gmail.com>
2023-06-13 22:23:06 +0300

    .

--- Terraform/SG/main.tf
+++ Terraform/SG/main.tf
@@ -1,4 +1,4 @@
-data "terraform_remote_state" "VPC" {
+data "terraform_remote_state" "vps" {
   backend = "s3"
   config = {
     bucket = "mainacademy-project-terraform-back"
@@ -12,7 +12,7 @@ data "terraform_remote_state" "VPC" {
 resource "aws_security_group" "my_security_group" {
   name        = "${var.name}"
   description = "${var.description}"
-  vpc_id      = data.terraform_remote_state.VPC.outputs.vps
+  vpc_id      = "${data.terraform_remote_state.vps.vpc_id}"
 
   ingress {
     from_port   = 22
