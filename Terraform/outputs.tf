a82b7f2cf232138f3427a6169f37ddb781da7f0b
GnoM <gnomovi4@gmail.com>
2023-06-14 22:56:53 +0300

    .

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket = "mainacademy-project-terraform-back"
+    key    = "dev/outputs/terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
+
+
+
 output "vpc_id" {
   value = aws_vpc.my_vpc.id
 }
