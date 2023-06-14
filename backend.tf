51c9818d6ad702060855e92238b1603e8025b279
GnoM <gnomovi4@gmail.com>
2023-06-14 22:44:18 +0300

    .

---
+++ backend.tf
@@ -0,0 +1,11 @@
+
+
+/*
+terraform {
+  backend "s3" {
+    bucket = "mainacademy-project-terraform-back"
+    key    = "dev/main/terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
+*/
--- main.tf
+++ main.tf
@@ -2,13 +2,6 @@ provider "aws" {
   region = "eu-central-1" 
 }
 
-terraform {
-  backend "s3" {
-    bucket = "mainacademy-project-terraform-back"
-    key    = "dev/main/terraform.tfstate"
-    region = "eu-central-1"
-  }
-}
 
 /*
 module "VPC" {
