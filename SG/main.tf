691d3b31b7ea140138df188ea4600c8b699506ba
GnoM <gnomovi4@gmail.com>
2023-06-15 16:52:01 +0300

    .

---
+++ SG/main.tf
@@ -0,0 +1,48 @@
+
+data "terraform_remote_state" "backend_outputs" {
+  backend = "s3"
+  config = {
+    bucket = "mainacademy-project-terraform-back"
+    key    = "dev/backend/terraform.tfstate"
+    region = "eu-central-1"
+   }
+}
+
+output "backend_variables" {
+    value = data.terraform_remote_state.backend_outputs.outputs
+}
+
+
+/*
+resource "aws_security_group" "my_security_group" {
+  name        = "${var.name}"
+  description = "${var.description}"
+  vpc_id      = var.vpc_id
+  tags = {
+    Name = "my-security-group"
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  
+}
+*/
--- main.tf
+++ main.tf
@@ -7,5 +7,9 @@ module "VPC" {
 }
 
 
+module "SG" {
+  source = "./SG" #SG Module PATH
+}
+
 
 
