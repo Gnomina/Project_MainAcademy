c9bf7a4fa2564d7bc2f4a389c9f2a48173ebe795
GnoM <gnomovi4@gmail.com>
2023-06-15 22:38:12 +0300

    .

--- Instance/variables.tf
+++ Instance/variables.tf
@@ -3,7 +3,7 @@ variable "ami_id"{
     description = "Canonical, Ubuntu, 20.04 LTS, amd64 focal image build on 2023-05-17"
 }
 
-variables "instance_type"{
+variable "instance_type"{
   default     = "t2.micro"
     description = "Free tire eligible t2.mikro instance"
 }
