5e167426ee55538b3b0a0b0dfac2492a8eb5303f
GnoM <gnomovi4@gmail.com>
2023-06-15 14:37:35 +0300

    .

--- outputs.tf
+++ outputs.tf
@@ -2,5 +2,10 @@
 
 output "vpc_id_VPC" {
   value = module.VPC.vpc_id_VPC
-  description = "value of vpc_id_main from VPC module (outputs_source = ./VPC)"
+  description = "value of vpc_id_VPC from VPC module (outputs_source = ./VPC)"
+}
+
+output "subnet_id_VPC" {
+  value = module.VPC.subnet_id_VPC
+  description = "value of subnet_id_VPC from VPC module (outputs_source = ./VPC)"
 }
