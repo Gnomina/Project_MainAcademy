f01569cbc6c30b801fb8fbcfdc188544439eed5b
GnoM <gnomovi4@gmail.com>
2023-06-21 15:44:54 +0300

    .

--- ECR/main.tf
+++ ECR/main.tf
@@ -2,5 +2,8 @@ resource "aws_ecr_repository" "my_repository" {
   name = "${var.ECR_Repo_Name}"  
 
   image_tag_mutability = "${var.Tag_Mutability}" 
-  scan_on_push         = true       # Опционально, активируйте сканирование образов при пуше
+  
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 }
