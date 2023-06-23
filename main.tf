40c3ff6ce5df1a829a6296026db7ad5f45a907b4
GnoM <gnomovi4@gmail.com>
2023-06-23 17:19:52 +0300

    romove iam_role

--- main.tf
+++ main.tf
@@ -11,9 +11,6 @@ module "SG" { #Include SG module
   vpc_id = module.VPC.vpc_id_out #pass the local value of the "vpc_id_out" variable from the VPC module to the SG module
 }
 
-module "IAM_Role" { #Include IAM_Role module
-  source = "./IAM_Role" #IAM_Role Module PATH
-}
 
 
 module "Instance" { #Include Instance module
@@ -22,5 +19,9 @@ module "Instance" { #Include Instance module
   subnet_id = module.VPC.subnet_id_out #pass the local value of the "subnet_id_out" variable from the VPC module to the Instance module
 }
 
+
+module "ECR"{
+  source = "./ECR"
+}
 
 
