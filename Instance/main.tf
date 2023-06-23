b752ee5de064a6206415559fe7f4da0746e67f4e
GnoM <gnomovi4@gmail.com>
2023-06-23 17:21:14 +0300

    .

--- Instance/main.tf
+++ Instance/main.tf
@@ -18,15 +18,9 @@ resource "aws_instance" "example"{
   vpc_security_group_ids = ["${var.sucurity_group}"]
   subnet_id              = "${var.subnet_id}"
   associate_public_ip_address = true
-  iam_instance_profile = "${var.polisy_arn}"
   tags = {
     Name = "MainAcademy_Instance_TEST"
   } 
-
-  #iam_instance_profile {
-   # name = "${var.Iam_Role_Name}"
-  #}
-                   
 }
 
 
