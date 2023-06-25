348d58b1f958b617a254bf5984586f9b992ecaa3
GnoM <gnomovi4@gmail.com>
2023-06-25 20:37:31 +0300

    .

--- variables.tf
+++ variables.tf
@@ -4,3 +4,14 @@ variable "Region" { # Local variable
     default = "eu-central-1"
 }
 
+variable "instance_count" {
+  description = "Количество запускаемых инстансов"
+  type        = number
+  default     = 1
+}
+
+variable "instance_tag" {
+  description = "Тег для назначения инстансам"
+  type        = string
+  default     = "Non_Tag"
+}
