7e051f2c348db41cad320c8f9695af817200e9d6
GnoM <gnomovi4@gmail.com>
2023-06-25 20:26:26 +0300

    .

--- Instance/outputs.tf
+++ Instance/outputs.tf
@@ -1,10 +1,18 @@
 
 #--------------Instance Outputs-----------------
 
-output "instance_public_ip" { # Local output variable
-  value = aws_instance.example.public_ip # Public IP that the instance receives from AWS. output variable.
- }
+#output "instance_public_ip" { # Local output variable
+#  value = aws_instance.example.public_ip # Public IP that the instance receives from AWS. output variable.
+# }
 
- output "instance_id" { # Local output variable
-  value = aws_instance.example.id # Instance ID that the instance receives from AWS. output variable.}
+# output "instance_id" { # Local output variable
+#  value = aws_instance.example.id # Instance ID that the instance receives from AWS. output variable.}
+#}
+
+output "instance_public_ip" {
+  value = [for instance in aws_instance.example : instance.public_ip]
+}
+
+output "instance_id" {
+  value = [for instance in aws_instance.example : instance.id]
 }
