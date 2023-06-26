54849c18a330e0e6295eaf8b743edf29a02e8fe8
GnoM <gnomovi4@gmail.com>
2023-06-26 14:26:04 +0300

    .

--- s3bucket/main.tf
+++ s3bucket/main.tf
@@ -1,10 +1,13 @@
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "test-dev-bucket" # Замените на имя вашего бакета
-
-  acl    = "private" # Настройте права доступа в соответствии с вашими потребностями
-
+ 
   tags = {
     Name        = "dev_bucket"
     Environment = "dev"
   }
+}
+resource "aws_s3_bucket_acl" "my_bucket_acl" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  acl = "private" # Настройте права доступа в соответствии с вашими потребностями
 }
