f4ea6f8b7ab23069b30f2447d83cab2d8a7d1cd0
GnoM <gnomovi4@gmail.com>
2023-06-26 14:40:26 +0300

    .

--- s3bucket/main.tf
+++ s3bucket/main.tf
@@ -1,8 +1,17 @@
 resource "aws_s3_bucket" "my_bucket_test" {
-  bucket = "dev-bucket" # Замените на имя вашего бакета
+  bucket = "mainacademy-dev" # Замените на имя вашего бакета
  
   tags = {
     Name        = "dev_bucket"
     Environment = "dev"
+  }
+}
+
+resource "aws_s3_bucket" "my_bucket_test" {
+  bucket = "mainacademy-prod" # Замените на имя вашего бакета
+ 
+  tags = {
+    Name        = "prod_bucket"
+    Environment = "prod"
   }
 }
