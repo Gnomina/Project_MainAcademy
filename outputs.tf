81b648321b11d33bfa87e3cac7043cec2504cc17
GnoM <gnomovi4@gmail.com>
2023-06-26 14:23:55 +0300

    .

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 
-
+/*
 output "vpc_id" {
   value = module.VPC.vpc_id_out #this variable locate in VPC module -> outputs 
   description = "value of vpc_id_VPC from VPC module (outputs_source = ./VPC)"
 }
-
+*/
