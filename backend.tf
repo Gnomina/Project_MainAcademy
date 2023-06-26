48edb962fac605e7c42063e36817deab2af11039
GnoM <gnomovi4@gmail.com>
2023-06-26 14:17:09 +0300

    .

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 #Module for save backend data on s3 bucket.
 terraform {
   backend "s3" {
-    bucket = "mainacademy-project-terraform-back" #name of bucket
-    key    = "dev/backend/terraform.tfstate" #path to file
+    bucket = "test-buk-g" #name of bucket
+    key    = "test-buk-g/dev/backend/terraform.tfstate" #path to file
     region = "eu-central-1" #region
   }
 }
