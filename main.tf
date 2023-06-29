f97fd73797d0dde0b4add32a14b581b6e3851f5b
GnoM <gnomovi4@gmail.com>
2023-06-29 22:06:46 +0300

    .

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "aws_s3_bucket" "site_origin" {
 resource "aws_s3_bucket_public_access_block" "site_origin" {
   bucket = aws_s3_bucket.site_origin.bucket
 
-  block_public_acls       = true
+  block_public_acls       = false
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
