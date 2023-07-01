620fe97c780a3869becaf9b410fa241ed96e7747
GnoM <gnomovi4@gmail.com>
2023-07-01 18:11:57 +0300

    .

--- main.tf
+++ main.tf
@@ -212,7 +212,7 @@ resource "aws_s3_bucket_policy" "s3_polisy"{
   ]
 
   bucket = aws_s3_bucket.site_prod.id
-  policy = data.aws_iam_policy_document.site_prod.json
+  policy = data.aws_iam_policy_document.s3_polisy.json
   //policy = data.aws_iam_policy_document.site_origin.json
 }
 /*
