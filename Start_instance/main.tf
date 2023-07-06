2e657d9fed25290ae27f36d8588bea6ed62062ff
GnoM <gnomovi4@gmail.com>
2023-07-06 19:57:28 +0300

    .

--- Start_instance/main.tf
+++ Start_instance/main.tf
@@ -3,9 +3,9 @@ provider "aws" {
 }
 
 module "VPC" { #Include VPC module
-  source = "./VPC" #VPC Module PATH
+  source = "./VPC/" #VPC Module PATH
 }
-
+/*
 module "SG" { #Include SG module
   source = "./SG" #SG Module PATH
   vpc_id = module.VPC.vpc_id_out #pass the local value of the "vpc_id_out" variable from the VPC module to the SG module
@@ -16,3 +16,4 @@ module "Instance" { #Include Instance module
   sucurity_group = module.SG.security_group_id_out #pass the local value of the "security_group_id" variable from the SG module to the Instance module
   subnet_id = module.VPC.subnet_id_out #pass the local value of the "subnet_id_out" variable from the VPC module to the Instance moduleъ
 }
+*/
