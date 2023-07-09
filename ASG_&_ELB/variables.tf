5b4ea148aa2f15d2b633701d4980b04a6e2aa23f
GnoM <gnomovi4@gmail.com>
2023-07-09 17:59:29 +0300

    .

--- ASG_&_ELB/variables.tf
+++ ASG_&_ELB/variables.tf
@@ -3,11 +3,12 @@ variable "Region" { # Local variable
     description = "Region"
     default = "eu-central-1"
 }
-
+/*
 variable "ami_id" {
   description = "AMI ID for the EC2 instances"
   type        = string
 }
+*/
 
 variable "subnet_id" {
   description = "Subnet ID for the EC2 instances"
