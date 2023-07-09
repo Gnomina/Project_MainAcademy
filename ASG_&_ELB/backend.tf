8ce2f0ef630dbf2272d1cf2c27182683ea049550
GnoM <gnomovi4@gmail.com>
2023-07-09 18:17:48 +0300

    .

--- ASG_&_ELB/backend.tf
+++ ASG_&_ELB/backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "s3" {
     bucket = "mainacademy-backend" #name of bucket
-    key    = "app_stack_backend/terraform.tfstate" #path to file
+    key    = "app_stack_backend_asg/terraform.tfstate" #path to file
     region = "eu-central-1" #region
   }
 }
