1e440aa72b51912ca88a001589dc62c8f6d9d90c
GnoM <gnomovi4@gmail.com>
2023-07-09 16:47:17 +0300

    .

--- ASG_&_ELB/variables.tf
+++ ASG_&_ELB/variables.tf
@@ -3,3 +3,29 @@ variable "Region" { # Local variable
     description = "Region"
     default = "eu-central-1"
 }
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instances"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "Subnet ID for the EC2 instances"
+  type        = string
+}
+
+variable "sg_id" {
+  description = "Security Group ID for the EC2 instances"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID for the EC2 instances"
+  type        = string
+}
+
+variable "subnet_name" {
+  description = "Subnet name for the EC2 instances"
+  type        = string
+}
+
